1128c1e1dfb9ae776a72feed4faf143b3647374f
Kateriina <blondinkova@mail.ru>
2021-03-21 20:29:59 +0300

    add second activity(signin_layout)

--- app/src/main/java/com/example/lesson2/MainActivity.kt
+++ app/src/main/java/com/example/lesson2/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.signin_layout)
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
 }
